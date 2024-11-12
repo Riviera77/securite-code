ad2b61925955b2d38edbe901ccca4151e2879b86
Riviera77 <103321314+Riviera77@users.noreply.github.com>
2024-11-12 16:27:42 +0100

    lab-02

---
+++ lab-02/script.js
@@ -0,0 +1,5 @@
+function displayText() {
+	const text = document.getElementById('userText').value;
+	const sanitizedText = DOMPurify.sanitize(text);
+	document.getElementById('output').textContent = sanitizedText;
+}
